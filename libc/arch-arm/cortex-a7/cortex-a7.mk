5b105ef8d662e7222b3a9fb16baefb7abb10ac66
Dave Kessler <activethrasher00@gmail.com>
2015-09-24 21:22:14 -0400

    add cortex-a15 memchr.S assembly for cortex-a7
    Change-Id: I28a4b2388d3dfb6879827f3a3d37d21131cc25f2

--- libc/arch-arm/cortex-a7/cortex-a7.mk
+++ libc/arch-arm/cortex-a7/cortex-a7.mk
@@ -2,6 +2,7 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a7/bionic/memset.S \
 
 libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/memchr.S \
     arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
